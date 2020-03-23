8654a46dc0289abee8034bf6d62c06eec51cc844
Stephen Healey <s_healey@hotmail.co.uk>
2020-03-23 19:27:40 +0000

    Added app scaffolding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: 'home', component: LandingComponent },
+  { path: '**', component: LandingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
