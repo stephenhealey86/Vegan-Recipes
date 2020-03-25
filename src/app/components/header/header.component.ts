c7ca625b914f0a03a0d88989bf251c3dc2e2201d
Stephen Healey <s_healey@hotmail.co.uk>
2020-03-25 09:49:00 +0000

    Started instructions layout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,4 +12,23 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
   }
 
+  public animate(event: MouseEvent): void {
+    try {
+      const LINK = event.srcElement as HTMLLinkElement;
+      const ICON = LINK.children[0] as HTMLElement;
+      ICON.animate([
+        // keyframes
+        { transform: 'rotate(0)' },
+        { transform: 'rotate(360deg)' },
+        { transform: 'rotate(0)' }
+      ], {
+        // timing options
+        duration: 2000,
+        iterations: 1
+      });
+          } catch (error) {
+          console.log(error);
+          }
+        }
+
 }
