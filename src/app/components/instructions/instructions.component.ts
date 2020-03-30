01ef87a84e69f537e808b7fdc33afc454e6bf2d1
Stephen Healey <s_healey@hotmail.co.uk>
2020-03-30 18:17:14 +0100

    Refactored instructions component.ts

--- src/app/components/instructions/instructions.component.ts
+++ src/app/components/instructions/instructions.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RecipesService } from 'src/app/services/recipes.service';
-import { ActivatedRoute } from '@angular/router';
-import { SpoonacularInformationResult } from 'src/app/models/spoonacular-information-result';
-import { Observable, of } from 'rxjs';
+import { ActivatedRoute, Params } from '@angular/router';
+import { concatMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-instructions',
@@ -11,25 +10,13 @@ import { Observable, of } from 'rxjs';
 })
 export class InstructionsComponent implements OnInit {
 
-  recipeInstructions$: Observable<SpoonacularInformationResult>;
+  recipeInstructions$ = this.route.paramMap
+    .pipe(
+      concatMap((p: Params) => this.recipeService.getRecipeInstructions(p.params.id))
+    );
 
   constructor(private recipeService: RecipesService, private route: ActivatedRoute) { }
 
-  ngOnInit() {
-    this.getRecipeInstructions(this.getRecipeId());
-  }
-
-  private getRecipeInstructions(id: string): void {
-    this.recipeInstructions$ = this.recipeService.getRecipeInstructions(id);
-  }
-
-  private getRecipeId(): string {
-    let id: string;
-    this.route.paramMap
-      .subscribe(p => {
-        id = p.get('id');
-      });
-    return id;
-  }
+  ngOnInit() {}
 
 }
